885d5aa7e2d7295e70d3da19ead0dad8cf9e410b
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-06 15:51:22 -0600

    added playername func

--- play.js
+++ play.js
@@ -57,4 +57,8 @@ class Game {
         const playerNameEl = document.querySelector('.player-name');
         playerNameEl.textContent = this.getPlayerName();
     }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Mystery player';
+    }
 }
