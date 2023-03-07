999abc877cacb9ba54a8c7cbaeff6ead6deeb6a5
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-06 19:52:13 -0600

    referred to orginal to commit changes

--- scores.js
+++ scores.js
@@ -1,30 +1,30 @@
 function loadScores() {
     let scores = [];
     const scoresText = localStorage.getItem('scores');
-    if(scoresText) {
-        scores = JSON.parse(scoresText);
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
     }
     const tableBodyEl = document.querySelector('#scores');
-    if(scores.length) {
-        for(const [a, score] of scores.entries()) {
-            const positionTdEl = document.createElement('td');
-            const nameTdEl = document.createElement('td');
-            const scoreTdEl = document.createElement('td');
-            const dateTdEl = document.createElement('td');
-            positionTdEl.textContent = a + 1;
-            nameTdEl.textContent = score.name;
-            scoreTdEl.textContent = score.score;
-            scoreTdEl.textContent = score.date;
-            const rowEl = document.createElement('tr');
-            rowEl.appendChild(positionTdEl);
-            rowEl.appendChild(nameTdEl);
-            rowEl.appendChild(scoreTdEl);
-            rowEl.appendChild(dateTdEl);
-            tableBodyEl.appendChild(rowEl);
-        }
+    if (scores.length) {
+      for (const [a, score] of scores.entries()) {
+        const positionTdEl = document.createElement('td');
+        const nameTdEl = document.createElement('td');
+        const scoreTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+        positionTdEl.textContent = a + 1;
+        nameTdEl.textContent = score.name;
+        scoreTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(positionTdEl);
+        rowEl.appendChild(nameTdEl);
+        rowEl.appendChild(scoreTdEl);
+        rowEl.appendChild(dateTdEl);
+        tableBodyEl.appendChild(rowEl);
+      }
     } else {
-        tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score!</td></tr>';
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
     }
-}
-
-loadScores();
+  }
+  
+  loadScores();
