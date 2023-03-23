91f1924228f03190a80912deef40121cc81d6b3b
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-22 22:44:46 -0500

    added displayQuote func in aboutjs

--- public/about.js
+++ public/about.js
@@ -7,5 +7,27 @@ function displayPicture() {
             const width = containerEl.offsetWidth;
             const height = containerEl.offsetHeight;
             const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
-        })
+            const imgEl = document.createElement('img');
+            imgEl.setAttribute('src', imgUrl);
+            containerEl.appendChild(imgEl);
+        });
 }
+
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+            const containerEl = document.querySelector('#quote');
+            const quoteEl = document.createElement('p');
+            quoteEl.classList.add('quote');
+            const authorEl = document.createElement('p');
+            authorEl.classList.add('author');
+            quoteEl.textContent = data.content;
+            authorEl.textContent = data.author;
+            containerEl.appendChild(quoteEl);
+            containerEl.appendChild(authorEl);
+        });
+}
+
+displayPicture();
+displayQuote();
