f71e127be6a48621ca71d5511c74c3e95151bdec
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-22 20:15:56 -0500

    updated error msg to follow new filename

--- deployService.sh
+++ deployService.sh
@@ -11,7 +11,7 @@ done
 
 if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployService.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
