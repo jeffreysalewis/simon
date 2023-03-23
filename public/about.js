af388ccd0a22ab26572264606feed5fb88e71786
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-22 21:02:40 -0500

    added displayPicture func

---
+++ public/about.js
@@ -0,0 +1,11 @@
+function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+        .then((response) => response.json())
+        .then((data) => {
+            const containerEl = document.querySelector('#picture');
+            const width = containerEl.offsetWidth;
+            const height = containerEl.offsetHeight;
+            const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        })
+}
