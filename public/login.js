d16f73f305b0d823312e4131ba15b8928095fca0
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-27 23:14:46 -0500

    fixed typos

--- public/login.js
+++ public/login.js
@@ -1,77 +1,78 @@
 (async () => {
     let authenticated = false;
     const userName = localStorage.getItem('userName');
-    if(userName) {
-        const nameEl = document.querySelector('#userName');
-        nameEl.value = userName;
-        const user = await getUser(nameEl.value);
-        authenticated = user?.authenticated;
+    if (userName) {
+      const nameEl = document.querySelector('#userName');
+      nameEl.value = userName;
+      const user = await getUser(nameEl.value);
+      authenticated = user?.authenticated;
     }
-
-    if(authenticated) {
-        document.querySelector('#playerName').textContent = userName;
-        setDisplay('loginControls', 'none');
-        setDisplay('playControls', 'block');
+  
+    if (authenticated) {
+      document.querySelector('#playerName').textContent = userName;
+      setDisplay('loginControls', 'none');
+      setDisplay('playControls', 'block');
     } else {
-        setDisplay('loginControls', 'block');
-        setDisplay('playControls', 'none');
+      setDisplay('loginControls', 'block');
+      setDisplay('playControls', 'none');
     }
-})();
-
-async function loginUser() {
+  })();
+  
+  async function loginUser() {
     loginOrCreate(`/api/auth/login`);
-}
-
-async function createUser() {
+  }
+  
+  async function createUser() {
     loginOrCreate(`/api/auth/create`);
-}
-
-async function loginOrCreate(endpoint) {
+  }
+  
+  async function loginOrCreate(endpoint) {
     const userName = document.querySelector('#userName')?.value;
     const password = document.querySelector('#userPassword')?.value;
     const response = await fetch(endpoint, {
-        method: 'post',
-        body: JSON.stringify({ email: userName, password: password }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     });
     const body = await response.json();
-
-    if(response?.status === 200) {
-        localStorage.setItem('userName', userName);
-        window.location.href = 'play.html';
+  
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'play.html';
     } else {
-        const modalEl = document.querySelector('#msgModal');
-        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
-        const msgModal = new bootstrap.Modal(modalEl, {});
-        msgModal.show();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
     }
-}
-
-function play() {
-    window.location.href = "play.html";
-}
-
-function logout() {
+  }
+  
+  function play() {
+    window.location.href = 'play.html';
+  }
+  
+  function logout() {
     fetch(`/api/auth/logout`, {
-        method: 'delete',
+      method: 'delete',
     }).then(() => (window.location.href = '/'));
-}
-
-async function getUser(email) {
+  }
+  
+  async function getUser(email) {
     let scores = [];
-    //see if we have a user w/ the given email
+    // See if we have a user with the given email.
     const response = await fetch(`/api/user/${email}`);
-    if(response.status === 200) {
-        return response.json();
+    if (response.status === 200) {
+      return response.json();
     }
+  
     return null;
-}
-
-function setDisplay(controlId, display) {
+  }
+  
+  function setDisplay(controlId, display) {
     const playControlEl = document.querySelector(`#${controlId}`);
-    if(playControlEl) {
-        playControlEl.style.display = display;
+    if (playControlEl) {
+      playControlEl.style.display = display;
     }
-}
+  }
