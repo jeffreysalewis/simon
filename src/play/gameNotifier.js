cf2fd253cdafb9e0bf6d52bb72c38d138c859d3f
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-08 13:55:20 -0500

    finished gamenotifier js

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -24,5 +24,37 @@ class GameEventNotifier {
         }
 
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket.onopen = (event) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
+    }
+
+    broadcastEvent(from, type, value) {
+        const event = new EventMessage(from, type, value);
+        this.socket.send(JSON.stringify(event));
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers.filter((h) => h !== handler);
+    }
+
+    receiveEvent(event) {
+        this.event.push(event);
+
+        this.events.forEach((e) => {
+            this.handlers.forEach((handler) => {
+                handler(e);
+            });
+        });
     }
 }
+
+const GameNotifier = new GameEventNotifier();
+export { GameEvent, GameNotifier };
