133078568a52d05a748b30f882dda30c43ba8c94
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-08 14:12:30 -0500

    fixed error msg

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -2,66 +2,66 @@ const GameEvent = {
     System: 'system',
     End: 'gameEnd',
     Start: 'gameStart',
-};
-
-class EventMessage {
-    constructor(from, type, value){
-        this.from = from;
-        this.type = type;
-        this.value = value;
+  };
+  
+  class EventMessage {
+    constructor(from, type, value) {
+      this.from = from;
+      this.type = type;
+      this.value = value;
     }
-}
-
-class GameEventNotifier {
+  }
+  
+  class GameEventNotifier {
     events = [];
     handlers = [];
-
+  
     constructor() {
-        //when dev debugging we need to talk to the service and not the react debugger
-        let port = window.location.port;
-        if(process.env.NODE_ENV !== 'production') {
-            port = 3000;
-        }
-
-        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';\
-        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
-        this.socket.onopen = (event) => {
-            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'connected' }));
-        };
-        this.socket.onclose = {event} => {
-            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'dissconnected' }));
-        };
-        this.socket.onmessage = async (msg) => {
-            try {
-                const event = JSON.parse(await msg.data.text());
-                this.receiveEvent(event);
-            } catch {}
-        };
+      // When dev debugging we need to talk to the service and not the React debugger
+      let port = window.location.port;
+      if (process.env.NODE_ENV !== 'production') {
+        port = 3000;
+      }
+  
+      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+      this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+      this.socket.onopen = (event) => {
+        this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'connected' }));
+      };
+      this.socket.onclose = (event) => {
+        this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'disconnected' }));
+      };
+      this.socket.onmessage = async (msg) => {
+        try {
+          const event = JSON.parse(await msg.data.text());
+          this.receiveEvent(event);
+        } catch {}
+      };
     }
-
+  
     broadcastEvent(from, type, value) {
-        const event = new EventMessage(from, type, value);
-        this.socket.send(JSON.stringify(event));
+      const event = new EventMessage(from, type, value);
+      this.socket.send(JSON.stringify(event));
     }
-
+  
     addHandler(handler) {
-        this.handlers.push(handler);
+      this.handlers.push(handler);
     }
-
+  
     removeHandler(handler) {
-        this.handlers.filter((h) => h !== handler);
+      this.handlers.filter((h) => h !== handler);
     }
-
+  
     receiveEvent(event) {
-        this.event.push(event);
-
-        this.events.forEach((e) => {
-            this.handlers.forEach((handler) => {
-                handler(e);
-            });
+      this.events.push(event);
+  
+      this.events.forEach((e) => {
+        this.handlers.forEach((handler) => {
+          handler(e);
         });
+      });
     }
-}
-
-const GameNotifier = new GameEventNotifier();
-export { GameEvent, GameNotifier };
+  }
+  
+  const GameNotifier = new GameEventNotifier();
+  export { GameEvent, GameNotifier };  
