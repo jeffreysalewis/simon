188d11467fb5b124b3b47f91892b970bc87ed8be
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-08 01:54:34 -0500

    added app framework

--- src/app.jsx
+++ src/app.jsx
@@ -33,7 +33,78 @@ function App() {
 
     return (
         <div className='body bg-dark text-light'>
-            
+            <header className='container-fluid'>
+                <nav className='navbar fixed-top navbar-dark'>
+                    <div className='nabbar-brand'>
+                        Simon<sup>&reg;</sup>
+                    </div>
+                    <menu className='navbar-nav'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to="">
+                                Login
+                            </NavLink>
+                        </li>
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='play'>
+                                    Play
+                                </NavLink>
+                            </li>
+                        )}
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink classname='nav-link' to='scores'>
+                                    Scores
+                                </NavLink>
+                            </li>
+                        )}
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='about'>
+                                About
+                            </NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Login
+                            userName={userName}
+                            authState={authState}
+                            onAuthChange={(userName, authState) => {
+                                setAuthState(authState);
+                                setUserName(userName);
+                            }}
+                        />
+                    }
+                    exact
+                />
+                <Route path='/play' element={<Play userName={userName}/>}/>
+                <Route path='/scores' element={<Scores/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Routes>
+
+            <footer className='bg-dark text-dark text-muted'>
+                <div className='container-fluid'>
+                    <span className='text-reset'>Jeffrey Lewis</span>
+                    <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+                        Original Github
+                    </a>
+                    <a className='text-reset' href='https://github.com/jeffreysalewis/simon'>
+                        My Github
+                    </a>
+                </div>
+            </footer>
         </div>
     );
 }
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
